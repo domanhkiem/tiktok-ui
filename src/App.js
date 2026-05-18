5a32ed6f39cd736864ae5f0a14b78280e93dbd90
domanhkiem <domanhkiemnd@gmail.com>
2026-05-18 15:22:26 +0300

    Cài đặt và cấu hình Router/Layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from './components/Layout';
+import { Fragment } from 'react';
+
 function App() {
     return (
-        <div className="App">
-            <h1>React App</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((item, index) => {
+                        const Page = item.component;
+                        let Layout = DefaultLayout;
+                        if (item.layout) {
+                            Layout = item.layout;
+                        } else if (item.layout === null) {
+                            Layout = Fragment;
+                        }
+                        return (
+                            <Route
+                                key={index}
+                                path={item.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            ></Route>
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
